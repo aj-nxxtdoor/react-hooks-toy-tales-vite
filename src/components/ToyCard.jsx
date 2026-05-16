8741dbf6d7a1a63ea61b5767debc279ad67731bc
Abdifatah <abdifatah6002@gmail.com>
2026-05-17 00:12:54 +0100

    Fix fetch URLs to use relative paths

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({ toy, onDeleteToy, onLikeToy }) {
+  function handleLike() {
+    fetch(`/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ likes: toy.likes + 1 }),
+    })
+      .then((r) => r.json())
+      .then((updatedToy) => onLikeToy(updatedToy));
+  }
+
+  function handleDelete() {
+    fetch(`/toys/${toy.id}`, {
+      method: "DELETE",
+    }).then(() => onDeleteToy(toy.id));
+  }
+
   return (
     <div className="card" data-testid="toy-card">
-      <h2>{"" /* Toy's Name */}</h2>
-      <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
-        className="toy-avatar"
-      />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <h2>{toy.name}</h2>
+      <img src={toy.image} alt={toy.name} className="toy-avatar" />
+      <p>{toy.likes} Likes </p>
+    <button className="like-btn" onClick={handleLike}>Like {"<3"}</button>
+      <button className="del-btn" onClick={handleDelete}>Donate to GoodWill</button>
     </div>
   );
 }
